19d6774cc2d67e47a05c332ecfbecd4252734671
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2019-11-21 12:51:34 +0700

    startapp survey, create model, add forms and wired up

--- code/main/survey/forms.py
+++ code/main/survey/forms.py
@@ -6,20 +6,16 @@ from bootstrap_datepicker_plus import DatePickerInput
 from django.utils.translation import gettext_lazy as _
 
 class SurveyForm(forms.ModelForm):    
-    gender=forms.ChoiceField(
-        widget=forms.RadioSelect(),
-        label='เพศ',
-        choices=[('F','หญิง'),('M','ชาย')],
-    )
-    fruit=forms.ChoiceField(
-        widget=forms.RadioSelect(),
-        label='ผลไม้ที่ท่านชอบ',
-        choices=[('a','แอปเปิล'),('b','กล้วย'),('c','มะพร้าว'),('d','ทุเรียน')],
-    )
-
     class Meta:
         model=Survey
         fields="__all__"
+        labels={
+            'fullname': 'ชื่อ นามสกุล',
+            'gender':'เพศ',
+            'birthdate':'วันเดือนปีเกิด',
+            'fruit':'ผลไม้ที่ท่านชอบ',
+            'comment':'ข้อเสนอแนะ',
+        }
         widgets={
-            'birthdate':DatePickerInput(format='%Y-%m-%d')
+            'birthdate':DatePickerInput(format='%Y-%m-%d'),
         }
--- code/main/survey/models.py
+++ code/main/survey/models.py
@@ -5,7 +5,13 @@ from django.db import models
 class Survey(models.Model):
     fullname    = models.CharField(max_length=255,  blank=False)
     email       = models.EmailField(                blank=False)
-    gender      = models.CharField(max_length=1,    blank=False)
+    gender      = models.CharField(max_length=1,    blank=False,
+                            choices=[('F','หญิง'),('M','ชาย')],
+                            default='F',
+                        )
     birthdate   = models.DateField(auto_now=False,  blank=False)
-    fruit       = models.CharField(max_length=1,    blank=False)
+    fruit       = models.CharField(max_length=1,    blank=False,
+                            choices=[('a','แอปเปิ้ล'),('b','มะละกอ'),('c','กล้วย'),('d','ส้ม')],
+                            default='c',
+                        )
     comment     = models.TextField(                 blank=True)
